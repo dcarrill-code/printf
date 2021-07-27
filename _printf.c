546b4e12d68180580d0797ff1cc934d435aee611
dcarrill-code <3346@holbertonschool.com>
2021-07-27 01:45:41 +0000

    Updating .h file

---
+++ _printf.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _printf - this is our own version of printf it prints on the screen an array
+ * @format: is the string to print on the screen
+ *
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	int i;
+	var_list ls;
+
+	var_start(ls, format);
+	while(format[i])
+	{
+
+	}
+
+
+	var_end(ls);
+
+}
---
+++ holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include "stdarg.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+int _printf(const char *format, ...);
+
+
+#endif
