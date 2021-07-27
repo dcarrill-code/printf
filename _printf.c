6a2559238861e4d9b6cc7bc48e8c7f439bc03673
dcarrill-code <3346@holbertonschool.com>
2021-07-27 13:19:28 +0000

    Updating print code

--- _printf.c
+++ _printf.c
@@ -3,21 +3,60 @@
  * _printf - this is our own version of printf it prints on the screen an array
  * @format: is the string to print on the screen
  *
- *
+ * Return: returns the number of characters
  */
 
 int _printf(const char *format, ...)
 {
-	int i;
-	var_list ls;
+	int i, cont, printn;
+	va_list ls;
+	int sint[10];
+	char *temp;
 
-	var_start(ls, format);
-	while(format[i])
+	va_start(ls, format);
+	while (format[cont])
 	{
-
+		cont++;
 	}
 
+	while (format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch(*format)
+			{
+				case 'c':
+					*temp = va_arg(ls, int);
+					format++;
+				break;
 
-	var_end(ls);
+				case 's':
+					temp = va_arg(ls, char*);
+					format++;
+				break;
 
+				case 'd':
+					printn = va_arg(ls, int);
+					_itoa(printn, sint, 10);
+					format++;
+				break;
+
+				case 'i':
+					printn = va_arg(ls, int);
+					_itoa(printn, sint, 10);
+					format++;
+				break;
+
+				case '%':
+					temp[i] = va_arg(ls, int);
+					format++;
+				break;
+			}
+		}
+		format++;
+	}
+	va_end(ls);
+	printf("%s", temp);
+	return (cont);
 }
--- holberton.h
+++ holberton.h
@@ -1,8 +1,9 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-#include "stdarg.h"
-#include "stdlib.h"
-#include "stdio.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 
