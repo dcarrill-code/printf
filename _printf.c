2fe8aba8a2065ff46e08601e48e3d727f4185612
dcarrill-code <3346@holbertonschool.com>
2021-07-28 02:20:47 +0000

    Updating main

--- _printf.c
+++ _printf.c
@@ -8,21 +8,48 @@
 
 int _printf(const char *format, ...)
 {
-	int i, count;
-	char tmp;
+	unsigned int i = 0, flag, l = 0, st_len = 0, n1 = 0, n2 = 0;
+	char sc = '%';
 	va_list ls;
-
 
 	va_start(ls, format);
 
-	while(format[count])
+	while(format && *(format + i))
 	{
-		count++;
-	}
-
-	while(format[i])
-	{
-
+		flag = 1;
+		if (format[i] == sc && format[i + 1] == '\0')
+		{
+			return (-1);
+		}
+		else if ((format[i] == sc) &&
+				((format[i + 1] == sc) || (format[i + 1] == '\0')))
+		{
+			_putchar('%');
+			n1++;
+			flag = 0;
+			i++;
+		}
+		else if ((format[i] == sc) && (format[i + 1] != '\0')
+				&& (get_op_func(format[i + 1]) != NULL))
+		{
+			n2++;
+			flag = 0;
+			l = (get_op_func(format[i + 1]))(ap);
+			st_len += l;
+			i++;
+		}
+			if (flag)
+		{
+			_putchar(format[i]);
+		}
+		if (format[i] != '\0')
+			i++;
 	}
 	va_end(ls);
+	if (!format)
+	{
+		return (-1);
+	}
+	return (i - (n1 + 2 * n2) + st_len);
+
 }
