0f7d38d2ed1c3020b65a7ced60a699916d1140d8
root <3346@holbertonschool.com>
2021-07-28 20:17:05 -0500

    Test

--- holberton.h
+++ holberton.h
@@ -3,11 +3,16 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int *char_print(va_list p);
 int string_print(va_list p);
+int _printf(const char *format, ...);
+char *convnum(long int i, int b);
+
 /**
  * struct option - Function call structure.
  *
